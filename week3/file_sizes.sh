6b42e7d65f98e7d54b23a93a54554ddb08f2a87c
Jabez Wilson <jabez.wilson0@gmail.com>
2016-08-13 16:27:54 +1000

    finished file sizes script, with test folder

---
+++ week3/file_sizes.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+small=""
+med=""
+large=""
+
+for f in $( ls | sort ) ; do
+  num=$( wc -l < $f )
+  if [ $num -lt 10 ]; then
+    small="$small $f"
+  elif [ $num -lt 100 ]; then
+    med="$med $f"
+  else
+    large="$large $f"
+  fi
+done
+
+echo "Small files: $small"
+echo "Medium-sized files: $med"
+echo "Large files: $large"
