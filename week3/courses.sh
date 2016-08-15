195f5ef0f6414080ad5f3ba4bbee7e511b1f517e
Jabez Wilson <jabez.wilson0@gmail.com>
2016-08-15 13:48:15 +1000

    stuff

--- week3/courses.sh
+++ week3/courses.sh
@@ -10,6 +10,4 @@ under=`wget -q -O- "http://www.handbook.unsw.edu.au/vbook2016/brCoursesByAtoZ.js
   | egrep -i '^\s*<TD class="[a-z0-9]*"><A href="http://www\.handbook\.unsw\.edu\.au/undergraduate/courses/2016/([A-Z]{4}[0-9]{4})\.html">([ A-Za-z0-9-]*)</A></TD>$' \
   | sed -e 's/\s*<TD class=\"[a-zA-Z0-9]*\"><A href=\"http:\/\/www\.handbook\.unsw\.edu\.au\/undergraduate\/courses\/2016\/\([A-Z]\{4\}[0-9]\{4\}\)\.html\">\([ A-Za-z0-9-]*\)<\/A><\/TD>/\1 \2/' `
 
-
-
-printf "$under\n$post" | sort | uniq
+echo -e "$under\n$post" | sort | uniq
