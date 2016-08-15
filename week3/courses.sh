c6c32d95336480a6ced2ca14a5c24d4f7253305c
Jabez Wilson <jabez.wilson0@gmail.com>
2016-08-15 13:36:59 +1000

    completed it

--- week3/courses.sh
+++ week3/courses.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-under=`wget -q -O- "http://www.handbook.unsw.edu.au/vbook2016/brCoursesByAtoZ.jsp?StudyLevel=Undergraduate&descr=ALL" \
-  | egrep -i "<TD class=\"evenTableCell\"><A href=\"http://www.handbook.unsw.edu.au/undergraduate/courses/2016/([A-Z]{4}[0-9]{4}).html\">([ A-Za-z0-9]*)</A></TD>" \
-  | sed -e 's/\s*<TD class=\"evenTableCell\"><A href=\"http:\/\/www\.handbook\.unsw\.edu\.au\/undergraduate\/courses\/2016\/\([A-Z]\{4\}[0-9]\{4\}\)\.html\">\([ A-Za-z0-9]*\)<\/A><\/TD>/\1\ \2/' \
-  | egrep "$1" `
+post=`wget -q -O- "http://www.handbook.unsw.edu.au/vbook2016/brCoursesByAtoZ.jsp?StudyLevel=Postgraduate&descr=ALL"  \
+  | grep "$1" \
+  | egrep -i '^\s*<TD class="[a-z0-9]*"><A href="http://www\.handbook\.unsw\.edu\.au/postgraduate/courses/2016/([A-Z]{4}[0-9]{4})\.html">([ A-Za-z0-9-]*)</A></TD>$' \
+  | sed -e 's/\s*<TD class=\"[a-zA-Z0-9]*\"><A href=\"http:\/\/www\.handbook\.unsw\.edu\.au\/postgraduate\/courses\/2016\/\([A-Z]\{4\}[0-9]\{4\}\)\.html\">\([ A-Za-z0-9-]*\)<\/A><\/TD>/\1 \2/' `
 
-post=`wget -q -O- "http://www.handbook.unsw.edu.au/vbook2016/brCoursesByAtoZ.jsp?StudyLevel=Postgraduate&descr=ALL" \
-  | egrep -i "<TD class=\"evenTableCell\"><A href=\"http://www.handbook.unsw.edu.au/postgraduate/courses/2016/([A-Z]{4}[0-9]{4}).html\">([ A-Za-z0-9]*)</A></TD>" \
-  | sed -e 's/\s*<TD class=\"evenTableCell\"><A href=\"http:\/\/www\.handbook\.unsw\.edu\.au\/postgraduate\/courses\/2016\/\([A-Z]\{4\}[0-9]\{4\}\)\.html\">\([ A-Za-z0-9]*\)<\/A><\/TD>/\1\ \2/' \
-  | egrep "$1" `
+under=`wget -q -O- "http://www.handbook.unsw.edu.au/vbook2016/brCoursesByAtoZ.jsp?StudyLevel=Undergraduate&descr=ALL"  \
+  | grep "$1" \
+  | egrep -i '^\s*<TD class="[a-z0-9]*"><A href="http://www\.handbook\.unsw\.edu\.au/undergraduate/courses/2016/([A-Z]{4}[0-9]{4})\.html">([ A-Za-z0-9-]*)</A></TD>$' \
+  | sed -e 's/\s*<TD class=\"[a-zA-Z0-9]*\"><A href=\"http:\/\/www\.handbook\.unsw\.edu\.au\/undergraduate\/courses\/2016\/\([A-Z]\{4\}[0-9]\{4\}\)\.html\">\([ A-Za-z0-9-]*\)<\/A><\/TD>/\1 \2/' `
 
-echo "$under"
-echo "$post"
+
+
+printf "$under\n$post" | sort | uniq
