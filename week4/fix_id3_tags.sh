d1a217dedf587486929694ebb5b67f657888a37b
Jabez Wilson <jabez.wilson0@gmail.com>
2016-08-22 22:26:14 +1000

    ASAS

--- week4/fix_id3_tags.sh
+++ week4/fix_id3_tags.sh
@@ -4,6 +4,7 @@ for dir in "$@" ; do
   raw_dir=`echo "$dir" | sed -e 's/\/*$//' | rev | cut -d'/' -f'1' | rev`
   album=`echo "$raw_dir" | cut -d',' -f1`
   year=`echo "$raw_dir" | cut -d',' -f2 | tr -d ' '`
+  album="$album"," $year"
 
   #echo -e "album:$album\t\tyear:$year"
 
@@ -12,7 +13,7 @@ for dir in "$@" ; do
     track=`echo "$raw_f" | cut -d'-' -f1 | sed -e 's/\ *$//' | sed -e 's/^\ *//'`
     name=`echo "$raw_f" | cut -d'-' -f2 | sed -e 's/\ *$//' | sed -e 's/^\ *//'`
     artist=`echo "$raw_f" | cut -d'-' -f3 | sed -e 's/\ *$//' | sed -e 's/^\ *//' | sed -e 's/\.mp3//'`
-    # echo -e "album:$album\tyear:$year\ttrack:$track\tartist:$artist\tname:$name"
+    #echo -e "album:$album\tyear:$year\ttrack:$track\tartist:$artist\tname:$name"
     id3 -t "$name" -T "$track" -a "$artist" -A "$album" -y "$year" "$f"
   done
 
