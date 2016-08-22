eec2a949a987deb97141d2bd6ff7345b5fd769ef
Jabez Wilson <jabez.wilson0@gmail.com>
2016-08-22 20:32:57 +1000

    added script

---
+++ week4/email_images.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+#assuming all arguments are images
+#
+for f in $@ ; do
+  if [ -f "$f" ] ; then
+    display "$f"
+    echo -n "Address to e-mail this image to? "
+    read recipient
+    echo -n "Message to accompany image? "
+    read mes
+
+    mutt -s "$mes" -a "$f" -- "$recipient"
+  fi
+done
