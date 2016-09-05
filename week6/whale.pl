90fc485c1c8aaa09c6578376bc9d85e3ce31c138
Jabez Wilson <jabez.wilson0@gmail.com>
2016-09-05 21:26:40 +1000

    finished exercise 1

---
+++ week6/whale.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+use warnings;
+my $pods = 0;
+my $count = 0;
+my @arr = [];
+while(<STDIN>) {
+  chomp $_;
+  @arr = split(/ /,$_,2);
+  if ( $arr[1] eq  $ARGV[0] ) {
+    $pods = $pods + 1;
+    $count = $count + $arr[0];
+  }
+}
+print "$ARGV[0] observations: $pods pods, $count individuals\n";
