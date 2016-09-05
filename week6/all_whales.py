f3bc76e633bff594dfae5b8d3e77f82f2e314ff4
Jabez Wilson <jabez.wilson0@gmail.com>
2016-09-06 00:18:07 +1000

    finished all_whales.py

---
+++ week6/all_whales.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import sys
+import re
+
+mapPods = {}
+mapCount = {}
+for line in sys.stdin.read().strip().split('\n') :
+    line = line.strip()
+    line = re.sub(r'  +',' ',line)
+    arr = line.strip().split(' ', 2)
+
+    arr[1] = arr[1].lower()
+    arr[1] = re.sub(r' +$','',arr[1])
+    arr[1] = re.sub(r's$','',arr[1])
+    arr[1] = re.sub(r'  +',' ',arr[1])
+
+    if( not arr[1] in mapPods):
+        mapPods[arr[1]] = 0
+    if( not arr[1] in mapCount):
+        mapCount[arr[1]] = 0
+
+    mapPods[arr[1]] += 1
+    mapCount[arr[1]] += int(arr[0])
+
+for it in sorted(mapPods.keys()) :
+    print("%s observations: %d pods, %d individuals" % (it,mapPods[it], mapCount[it]))
