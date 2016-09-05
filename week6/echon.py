e93b5f289b3252d7ec1a14057890a79211032e84
Jabez Wilson <jabez.wilson0@gmail.com>
2016-09-05 23:40:54 +1000

    corrected echon

--- week6/echon.py
+++ week6/echon.py
@@ -6,5 +6,11 @@ if ( len(sys.argv) != 3) :
     print('Usage: ./echon.pl <number of lines> <string> at ./echon.pl line 3')
     sys.exit(1);
 
-for i in range(int(sys.argv[1])) :
+try:
+    num = int(sys.argv[1])
+except ValueError:
+    print('./echon.py: argument 1 must be a non-negative integer')
+    sys.exit(1)
+
+for i in range(num) :
     print(sys.argv[2]);
