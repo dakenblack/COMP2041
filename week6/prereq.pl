00fbf98d8815864dc1d4f129f4a6c813cb4083e4
Jabez Wilson <jabez.wilson0@gmail.com>
2016-09-06 11:05:16 +1000

    checked for different spelling of Pre-requisite:

--- week6/prereq.pl
+++ week6/prereq.pl
@@ -7,10 +7,10 @@ for my $degree (("postgraduate", "undergraduate")) {
   $url = "http://www.handbook.unsw.edu.au/$degree/courses/2015/$ARGV[0].html";
   open F, "wget -q -O- $url|" or die;
   while (my $line = <F>) {
-    if ( "$line" =~ /(<p>Prerequisite[^<]*<\/p>)/) {
+    if ( "$line" =~ /(<p>Pre-?[Rr]equisite[^<]*<\/p>)/) {
       my $tmp = $1;
-      while ($tmp =~ /([A-Z]{4}[0-9]{4})/g) {
-        push @result, $1 ;
+      while ($tmp =~ /([A-Z]{4}[0-9]{4})/gi) {
+        push @result, uc $1 ;
       }
     }
   }
