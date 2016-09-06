29c2b9e02b902afc4bcd1d1992ae0abe92594c39
Jabez Wilson <jabez.wilson0@gmail.com>
2016-09-06 11:11:35 +1000

    added check for p

--- week6/prereq.pl
+++ week6/prereq.pl
@@ -7,7 +7,7 @@ for my $degree (("postgraduate", "undergraduate")) {
   $url = "http://www.handbook.unsw.edu.au/$degree/courses/2015/$ARGV[0].html";
   open F, "wget -q -O- $url|" or die;
   while (my $line = <F>) {
-    if ( "$line" =~ /(<p>Pre-?[Rr]equisite[^<]*<\/p>)/) {
+    if ( "$line" =~ /(<p>[Pp]re-?[Rr]equisite[^<]*<\/p>)/) {
       my $tmp = $1;
       while ($tmp =~ /([A-Z]{4}[0-9]{4})/gi) {
         push @result, uc $1 ;
