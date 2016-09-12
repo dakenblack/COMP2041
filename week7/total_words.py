a16441c89faa24657b3b212fca8b2e007dba5ad0
Jabez Wilson <jabez.wilson0@gmail.com>
2016-09-13 00:08:54 +1000

    correction, I need sleep

--- week7/total_words.py
+++ week7/total_words.py
@@ -5,4 +5,4 @@ import re
 
 # http://stackoverflow.com/questions/16099694/how-to-remove-empty-string-in-a-list
 arr = list(filter(None, re.split(r'[^a-zA-Z]+',' '.join(sys.stdin.readlines()))));
-print(int(len(arr)) + " words");
+print(str(len(arr)) + " words");
