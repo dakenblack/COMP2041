00ca670dca707fc4ed55cbbeebbad26e8462faab
Jabez Wilson <jabez.wilson0@gmail.com>
2016-09-21 17:18:08 +1000

    prettified the tester

--- assignment1/test.sh
+++ assignment1/test.sh
@@ -4,17 +4,29 @@
 
 orig=`perl $1`
 code=`cat "$1" | perl plpy.pl`
-new=`python -c "$code"`
+new=`python -c "$code" 2>&1`
 dif=`diff <(echo "$orig") <(echo "$new")`
-divider="<=======================================>"
-echo "Converted Code:"
-echo "$code"
-echo "$divider"
-echo "Original Output: "
-echo "$orig"
-echo "$divider"
-echo "New Output:"
-echo "$new"
-echo "$divider"
-echo "Diff:"
-echo "$dif"
+
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+if [ -z "$dif" ] ; then
+  echo -e "${GREEN}<=================PASSED================>${NC}"
+else
+  echo -e "${RED}<=================FAILED================>${NC}"
+  divider="<=======================================>"
+  echo "$divider"
+  echo "Converted Code:"
+  echo "$code"
+  echo "$divider"
+  echo "Original Output: "
+  echo "$orig"
+  echo "$divider"
+  echo "New Output:"
+  echo "$new"
+  echo "$divider"
+  echo "Diff:"
+  echo "$dif"
+fi
