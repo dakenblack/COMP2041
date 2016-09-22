c8ada5b52e36ab017fbe9945c607babc0de59769
Jabez Wilson <jabez.wilson0@gmail.com>
2016-09-22 13:46:03 +1000

    added set 3 and changed dwn

--- assignment1/dwn.pl
+++ assignment1/dwn.pl
@@ -1,8 +1,15 @@
 #!/bin/perl
 
-@list = (5,6);
+@list = (
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/3/cookie0.input",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/3/cookie0.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/3/echo.2.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/3/echonl.0.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/3/five.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/3/prime0.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/3/tetrahedral.pl",
+);
 
 for $i (@list) {
-  `wget "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/2/answer$i.pl"`
+  `wget "$i"`
 }
-`wget "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/2/iota.pl"`
