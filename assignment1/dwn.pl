a3b7fbc2376050d5638cfd56de27162c3fa4744c
Jabez Wilson <jabez.wilson0@gmail.com>
2016-09-22 10:23:40 +1000

    set up for subset 2

--- assignment1/dwn.pl
+++ assignment1/dwn.pl
@@ -1,7 +1,8 @@
 #!/bin/perl
 
-@list = (0,1,2,3,4);
+@list = (5,6);
 
 for $i (@list) {
-  `wget "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/1/answer$i.pl"`
+  `wget "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/2/answer$i.pl"`
 }
+`wget "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/2/iota.pl"`
