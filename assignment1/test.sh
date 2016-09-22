9a8cc7de716e39b3b2b6511b23843cf981e74df0
Jabez Wilson <jabez.wilson0@gmail.com>
2016-09-22 10:56:25 +1000

    cooler test scripts

--- assignment1/test.sh
+++ assignment1/test.sh
@@ -2,10 +2,17 @@
 # @arg input file
 # assumes plpy.pl is in the current directory
 
-echo -ne "testing : $1 "
+suppress=0
+if [ $1 = "-s" ] ; then
+  file=$2
+  suppress=1
+else
+  file=$1
+fi
 
-orig=`perl $1`
-code=`perl plpy.pl $1`
+echo -ne "testing : $file "
+orig=`perl $file`
+code=`perl plpy.pl $file`
 new=`python -c "$code" 2>&1`
 dif=`diff <(echo "$orig") <(echo "$new")`
 
@@ -16,7 +23,7 @@ NC='\033[0m' # No Color
 
 if [ -z "$dif" ] ; then
   echo -e "${GREEN}<==============PASSED=============>${NC}"
-else
+elif [ "$suppress" -eq "0" ] ; then
   echo -e "${RED}<==============FAILED=============>${NC}"
   divider="<=================================>"
   echo "$divider"
@@ -31,4 +38,6 @@ else
   echo "$divider"
   echo "Diff:"
   echo "$dif"
+else
+  echo -e "${RED}<==============FAILED=============>${NC}"
 fi
