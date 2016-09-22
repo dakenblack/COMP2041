e195e2ff41e5e671eb2d63197afdf5901fe9007a
Jabez Wilson <jabez.wilson0@gmail.com>
2016-09-22 13:55:41 +1000

    added set4

--- assignment1/dwn.pl
+++ assignment1/dwn.pl
@@ -1,13 +1,14 @@
 #!/bin/perl
 
 @list = (
-  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/3/cookie0.input",
-  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/3/cookie0.pl",
-  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/3/echo.2.pl",
-  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/3/echonl.0.pl",
-  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/3/five.pl",
-  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/3/prime0.pl",
-  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/3/tetrahedral.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/4/devowel.arguments",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/4/devowel.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/4/echonl.1.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/4/line_count.1.input",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/4/line_count.1.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/4/odd0.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/4/size.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/4/size.pl",
 );
 
 for $i (@list) {
