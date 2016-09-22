e212df4ddc8b553b49472a81c88b57bda4e66af0
Jabez Wilson <jabez.wilson0@gmail.com>
2016-09-22 10:43:18 +1000

    started skeleton of the script

--- assignment1/test.sh
+++ assignment1/test.sh
@@ -1,34 +1,35 @@
-#!/bin/bash
+#!/usr/bin/perl
 # @arg input file
 # assumes plpy.pl is in the current directory
 
-echo -ne "testing : $1 "
+print "testing : $ARGV[0] ";
 
-orig=`perl $1`
-code=`perl plpy.pl $1`
-new=`python -c "$code" 2>&1`
-dif=`diff <(echo "$orig") <(echo "$new")`
+print "ASASAS\n";
+$orig=`perl $ARGV[0]`;
+$code=`perl plpy.pl $ARGV[0]`;
+$new=`python -c "$code" 2>&1`;
+$dif="as";
+print "$dif\n";
 
-
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-
-if [ -z "$dif" ] ; then
-  echo -e "${GREEN}<==============PASSED=============>${NC}"
-else
-  echo -e "${RED}<==============FAILED=============>${NC}"
-  divider="<=================================>"
-  echo "$divider"
-  echo "Converted Code:"
-  echo "$code"
-  echo "$divider"
-  echo "Original Output: "
-  echo "$orig"
-  echo "$divider"
-  echo "New Output:"
-  echo "$new"
-  echo "$divider"
-  echo "Diff:"
-  echo "$dif"
-fi
+$RED='\033[0;31m';
+$GREEN='\033[0;32m';
+$NC='\033[0m' ;
+# No Color
+if ( $dif eq "" ) {
+  print "${GREEN}<==============PASSED=============>${NC}\n";
+} else {
+  print "${RED}<==============FAILED=============>${NC}\n";
+  $divider="<=================================>";
+  print "$divider\n";
+  print "Converted Code:\n";
+  print "\Q$code\n";
+  print "$divider\n";
+  print "Original Output: \n";
+  print "$orig\n";
+  print "$divider\n";
+  print "New Output:\n";
+  print "$new\n";
+  print "$divider\n";
+  print "Diff:\n";
+  print "$dif\n";
+}
