f40bca041bb3be6cbd9803fc04222683bfb499d5
Jabez Wilson <jabez.wilson0@gmail.com>
2016-09-28 20:50:21 +1000

    added new files

--- assignment1/dwn.pl
+++ assignment1/dwn.pl
@@ -1,14 +1,13 @@
 #!/bin/perl
 
 @list = (
-  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/4/devowel.arguments",
-  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/4/devowel.pl",
-  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/4/echonl.1.pl",
-  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/4/line_count.1.input",
-  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/4/line_count.1.pl",
-  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/4/odd0.pl",
-  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/4/size.pl",
-  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/4/size.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/5/reverse_lines.0.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/5/reverse_lines.1.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/5/reverse_lines.2.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/5/reverse_lines.3.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/5/reverse_lines.4.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/5/reverse_lines.5.pl",
+  "http://www.cse.unsw.edu.au/~cs2041/assignments/plpy/examples/5/reverse_lines.6.pl",
 );
 
 for $i (@list) {
