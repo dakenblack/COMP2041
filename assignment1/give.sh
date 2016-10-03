788e2e4146c089111e484776cbf7cf31b872c3f1
Jabez Wilson <jabez.wilson0@gmail.com>
2016-10-03 20:43:00 +1100

    updated give script

--- assignment1/give.sh
+++ assignment1/give.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-
-give cs2041 ass1 plpy.pl diary.txt demos/demo?.pl tests/test?.pl tst tester stepper.pl
+give cs2041 ass1 plpy.pl diary.txt demos/demo0?.pl tests/test0?.pl tst tester stepper.pl
